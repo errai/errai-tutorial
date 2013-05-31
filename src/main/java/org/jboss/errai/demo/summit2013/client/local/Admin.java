cd0a5697be6cdf22046df8f44cf1cc2e3c184fd1
Christian Sadilek <christian.sadilek@gmail.com>
2013-05-31 15:21:04 -0400

    Introduced page navigations

---
+++ src/main/java/org/jboss/errai/demo/summit2013/client/local/Admin.java
@@ -0,0 +1,12 @@
+package org.jboss.errai.demo.summit2013.client.local;
+
+import org.jboss.errai.ui.nav.client.local.Page;
+import org.jboss.errai.ui.shared.api.annotations.Templated;
+
+import com.google.gwt.user.client.ui.Composite;
+
+@Page
+@Templated("Admin.html#app-template")
+public class Admin extends Composite {
+
+}
---
+++ src/main/java/org/jboss/errai/demo/summit2013/client/local/Complaint.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2009 JBoss, a division of Red Hat Hat, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.errai.demo.summit2013.client.local;
+
+import javax.inject.Inject;
+
+import org.jboss.errai.ui.nav.client.local.DefaultPage;
+import org.jboss.errai.ui.nav.client.local.Page;
+import org.jboss.errai.ui.nav.client.local.TransitionAnchor;
+import org.jboss.errai.ui.nav.client.local.TransitionTo;
+import org.jboss.errai.ui.shared.api.annotations.DataField;
+import org.jboss.errai.ui.shared.api.annotations.EventHandler;
+import org.jboss.errai.ui.shared.api.annotations.Templated;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+
+@Page(role = DefaultPage.class)
+@Templated("Complaint.html#app-template")
+public class Complaint extends Composite {
+
+	@Inject
+	@DataField
+	private Button submit;
+
+	@Inject
+	@DataField
+	private TransitionAnchor<Admin> admin;
+	
+    @Inject 
+    private TransitionTo<ComplaintSubmitted> complaintSubmittedPage;
+
+	@EventHandler("submit")
+	private void onSubmit(ClickEvent e) {
+		complaintSubmittedPage.go();
+	}
+	
+}
