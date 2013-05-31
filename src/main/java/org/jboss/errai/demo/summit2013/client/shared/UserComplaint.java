15113f3a1e8a2af86a0eb966c95168330fdd352d
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2013-05-31 16:46:23 -0400

    UserComplaint is a JPA @Entity

--- src/main/java/org/jboss/errai/demo/summit2013/client/shared/UserComplaint.java
+++ src/main/java/org/jboss/errai/demo/summit2013/client/shared/UserComplaint.java
@@ -1,8 +1,12 @@
 package org.jboss.errai.demo.summit2013.client.shared;
 
+import javax.persistence.Entity;
+import javax.persistence.Lob;
+
 import org.jboss.errai.common.client.api.annotations.Portable;
 import org.jboss.errai.databinding.client.api.Bindable;
 
+@Entity
 @Bindable
 @Portable
 public class UserComplaint
@@ -10,6 +14,19 @@ public class UserComplaint
    private String name;
    private String email;
    private String complaint;
+
+   @Lob
+   private String image;
+
+   public String getImage()
+   {
+      return image;
+   }
+
+   public void setImage(String image)
+   {
+      this.image = image;
+   }
 
    public String getName()
    {
