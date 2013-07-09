92e8d69a1bfaae364310fcc3dec88e9cc5390191
Jonathan Fuerth <jfuerth@gmail.com>
2013-07-08 21:07:21 -0400

    Added javadoc, explanatory comments, and decluttered use of @TransactionAttribute

--- src/main/java/org/jboss/errai/demo/summit2013/client/local/Admin.java
+++ src/main/java/org/jboss/errai/demo/summit2013/client/local/Admin.java
@@ -10,6 +10,7 @@ import javax.persistence.TypedQuery;
 import org.jboss.errai.common.client.api.RemoteCallback;
 import org.jboss.errai.common.client.util.LogUtil;
 import org.jboss.errai.demo.summit2013.client.shared.UserComplaint;
+import org.jboss.errai.demo.summit2013.server.rest.UserComplaintService;
 import org.jboss.errai.ioc.client.api.AfterInitialization;
 import org.jboss.errai.jpa.sync.client.local.ClientSyncManager;
 import org.jboss.errai.jpa.sync.client.shared.SyncResponse;
@@ -21,32 +22,59 @@ import org.jboss.errai.ui.shared.api.annotations.Templated;
 
 import com.google.gwt.user.client.ui.Composite;
 
+/**
+ * An ErraiUI templated page which shows the current set of complaints which have been submitted.
+ */
 @Page
 @Templated("Admin.html#app-template")
 public class Admin extends Composite {
+
+  /**
+   * The list of complaints that currently exist.
+   */
   @DataField
   private ListWidget<UserComplaint, ComplaintWidget> complaints;
 
+  /**
+   * The EntityManager that interacts with client-local storage.
+   */
   @Inject
   private EntityManager em;
 
   @Inject
   private App app;
 
+  /**
+   * The Errai Data Sync helper class which allows us to initiate a data synchronization with the server.
+   */
   @Inject
   private ClientSyncManager syncManager;
 
   public static boolean init;
-  
+
   public Admin() {
     complaints = new ComplaintListWidget("tbody");
   }
 
+  /**
+   * Queries the list of complaints from the browser's local storage, and
+   * displays the results in the {@code complaints} list widget.
+   */
   private void loadComplaints() {
     TypedQuery<UserComplaint> query = em.createNamedQuery("allComplaints", UserComplaint.class);
     complaints.setItems(query.getResultList());
   }
 
+  /**
+   * Receives UserComplaint objects that are fired as events either on the
+   * client or the server.
+   * <p>
+   * In this application, the {@link UserComplaintService} on the server
+   * broadcasts a UserComplaint event to all clients to indicate that somebody
+   * just updated that UserComplaint.
+   *
+   * @param created The UserComplaint object that was just created or updated on the server.
+   */
   @SuppressWarnings("unused")
   private void complaintChanged(@Observes UserComplaint created) {
     LogUtil.log("Got complaint from server:" + created);
@@ -58,6 +86,11 @@ public class Admin extends Composite {
     }
   }
 
+  /**
+   * Performs a full two-way data synchronization between the client and the
+   * server: the server gets all new and updated UserComplaint objects from us,
+   * and we get all new and updated UserComplaint objects from the server.
+   */
   @AfterInitialization
   private void sync() {
     Admin.init = true;
@@ -70,14 +103,14 @@ public class Admin extends Composite {
       }
     });
   }
-  
+
   @PageShown
   private void pageShown() {
     if (init) {
       sync();
     }
   }
-  
+
   private void flushToLocalStorage() {
     // TODO should be done by data sync manager internally
     syncManager.getDesiredStateEm().flush();
@@ -85,7 +118,7 @@ public class Admin extends Composite {
     syncManager.getExpectedStateEm().flush();
     syncManager.getExpectedStateEm().clear();
   }
-  
+
   private void mergeInLocalStorage(UserComplaint created) {
     syncManager.getExpectedStateEm().merge(created);
     syncManager.getExpectedStateEm().flush();
--- src/main/java/org/jboss/errai/demo/summit2013/server/rest/DataSyncServiceImpl.java
+++ src/main/java/org/jboss/errai/demo/summit2013/server/rest/DataSyncServiceImpl.java
@@ -14,27 +14,42 @@ import org.jboss.errai.jpa.sync.client.shared.SyncResponse;
 import org.jboss.errai.jpa.sync.client.shared.SyncableDataSet;
 import org.jboss.errai.jpa.sync.client.shared.UpdateResponse;
 
+/**
+ * An Errai RPC service which is called by the client when it wishes to
+ * synchronize the complaints data between itself and the server.
+ */
 @ApplicationScoped
 @Service
 public class DataSyncServiceImpl implements DataSyncService {
 
+  /**
+   * An EJB responsible for getting the JPA EntityManager and for transaction demarcation.
+   */
   @Inject
   private UserComplaintService userComplaintService;
-  
+
+  /**
+   * A CDI event source that fires UserComplaint instances to observers,
+   * both on the server and on clients.
+   */
   @Inject
   private Event<UserComplaint> updated;
 
   @Override
   public <X> List<SyncResponse<X>> coldSync(SyncableDataSet<X> dataSet, List<SyncRequestOperation<X>> remoteResults) {
-    List<SyncResponse<X>> response = userComplaintService.sync(dataSet, remoteResults); 
 
+    // First, let the UserComplaintService EJB do the sync (it gets the correct EntityManager and also handles transactions)
+    List<SyncResponse<X>> response = userComplaintService.sync(dataSet, remoteResults);
+
+    // Now fire a CDI event for each UserComplaint which was updated as a result of this sync
     for (SyncResponse<X> syncRequestResponse : response) {
       if (syncRequestResponse instanceof UpdateResponse) {
         UserComplaint newComplaint = (UserComplaint) ((UpdateResponse) syncRequestResponse).getEntity();
         updated.fire(newComplaint);
       }
     }
-    
+
+    // Finally, return the list of sync responses to the client, whose sync manager will update the client database
     return response;
   }
 }
