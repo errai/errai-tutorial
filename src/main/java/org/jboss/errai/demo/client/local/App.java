f5f2e16b8602d7348121d888790bbd42989865f9
Max Barkley <mbarkley@redhat.com>
2013-10-28 11:41:49 -0400

    Change rest endpoint to 'webapp' context

--- src/main/java/org/jboss/errai/demo/client/local/App.java
+++ src/main/java/org/jboss/errai/demo/client/local/App.java
@@ -37,7 +37,7 @@ public class App {
     // This is specifying the relative path to the REST endpoint used to store
     // complaints on the server. When compiling the native mobile app of this
     // demo, this needs to be changed to an absolute URL.
-    RestClient.setApplicationRoot("/errai-tutorial/rest");
+    RestClient.setApplicationRoot("/webapp/rest");
     // RestClient.setApplicationRoot("http://10.15.16.207:8080/errai-tutorial/rest");
   }
 
