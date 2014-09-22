d9853420b98bd1e3d0cfedadf99187af0cea7717
divd <divya.dadlani@gmail.com>
2014-09-22 16:11:29 -0400

    Updated Polymer components

---
+++ src/main/java/org/jboss/errai/demo/client/local/PaperButtonElement.java
@@ -0,0 +1,9 @@
+package org.jboss.errai.demo.client.local;
+
+import com.google.gwt.dom.client.Element;
+
+/**
+ * @author Divya Dadlani <ddadlani@redhat.com>
+ */
+public class PaperButtonElement extends Element {
+}
